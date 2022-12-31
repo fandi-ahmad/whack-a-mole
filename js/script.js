29b665474aa6445ee37200cd7031dc5e44101c86
fandi-ahmad <fandi4160@gmail.com>
2023-01-01 01:24:37 +0800

    redesign UI

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const
 ground = document.querySelectorAll('.ground'),
 mouse = document.querySelectorAll('.mouse'),
 scoreBoard = document.getElementById('scoreBoard'),
+scoreBoard2 = document.getElementById('scoreBoard2'),
 board = document.getElementById('board'),
 m1 = document.getElementById('m1'),
 m2 = document.getElementById('m2'),
@@ -9,7 +10,12 @@ m3 = document.getElementById('m3'),
 m4 = document.getElementById('m4'),
 m5 = document.getElementById('m5'),
 m6 = document.getElementById('m6'),
-time = document.getElementById('time');
+time = document.getElementById('time'),
+whiteBoard = document.getElementById('whiteBoard'),
+playBtn = document.getElementById('playBtn'),
+textHead = document.getElementById('textHead'),
+img = document.getElementById('img'),
+scoreEnd = document.getElementById('scoreEnd')
 
 let groundBefore;
 let endGame;
@@ -51,6 +57,9 @@ function startGame() {
     time.innerText = duration
     countDown()
     mouseShow()
+
+    whiteBoard.classList.add('d-none')
+
     setTimeout(() => {
         endGame = true
     }, 10000);
@@ -81,5 +90,12 @@ function countDown() {
     
     setTimeout(() => {
         clearInterval(td)
+        whiteBoard.classList.remove('d-none')
+
+        img.classList.add('d-none')
+        scoreEnd.classList.remove('d-none')
+
+        scoreBoard2.innerText = score
+        playBtn.innerText = 'Try Again'
     }, 10000);
 }
